7d2ef98207b03a29ec3160bc0792847ae9f2087d
genead <jirabona990@gmail.com>
2023-07-24 14:27:56 +0000

    done

--- src/main/java/SampleClassC.java
+++ src/main/java/SampleClassC.java
@@ -27,7 +27,10 @@ public class SampleClassC implements Comparable<SampleClassC>{
      * @param other another object of type SampleClassC, which we are comparing to 'this' object.
      */
     public int compareTo(SampleClassC other) {
-        return 0;
+        if(this.val < other.val){
+            return val *= -1;
+        }
+        return val;
     }
 
 }
